0917f50a852e97e54b3b7bd8e0c29675435f5fb0
Laura <dianazamora210@gmil.com>
2019-06-03 13:19:43 -0500

    barras

--- PDF/Multas.php
+++ PDF/Multas.php
@@ -1,8 +1,9 @@
 <?php
-
+include 'barcode.php';
 require('fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage('L', array(105,130));
+
 
 
 $pdf->image('../../PDF/logo2.png',5,5,20);
@@ -12,10 +13,15 @@ $Con = Conectar();
 $SQL ="SELECT MAX(Folio) FROM multas;";
 $Query = EjecutarConsulta($Con, $SQL);
 
+
    for ($I = 0;$I<mysqli_num_rows($Query);$I++)
 {
-    $Fila = mysqli_fetch_row($Query); 
-   $pdf->SetFont('Arial','B',12);   
+
+    $Fila = mysqli_fetch_row($Query);
+    $cod=$Fila[0];
+    barcode('../../PDF/'. $cod .'.png', $cod, 50, 'horizontal', 'code128', true);
+    
+   $pdf->SetFont('Arial','B',12);
    $pdf->Cell(0,4,utf8_decode('Secretaría de Seguridad Pública'),0,1,'R') ;
    $pdf->Cell(0,6,utf8_decode('y Tránsito Municipal'),0,1,'R') ;
    $pdf->SetFont('Arial','B',13);
@@ -30,14 +36,14 @@ $Query = EjecutarConsulta($Con, $SQL);
    $pdf->Cell(0,2,utf8_decode(' '),0,1,'L');
    $pdf->Cell(92,4,utf8_decode('Fecha de expedición '),0,0,'R') ; $pdf->SetFont('Arial','',9);$pdf-> Cell(0,4,$date,0,1,'R');
 }
-   
+
 $SQL2="SELECT * FROM vehiculos WHERE idVehiculo=$vehiculo";
 $Query2= EjecutarConsulta($Con, $SQL2);
 
    for ($J = 0;$J<mysqli_num_rows($Query2);$J++)
 {
-   $pdf->ln(); 
-   $Fila2= mysqli_fetch_row($Query2); 
+   $pdf->ln();
+   $Fila2= mysqli_fetch_row($Query2);
    $pdf->SetFont('Arial','B',9);$pdf->Cell(40,4,utf8_decode('Nombre  de Conductor:'),0,0,'L' );
    $pdf->SetFont('Arial','',9);
    $pdf->Cell(40,4,utf8_decode($Fila2[1]),0,1,'L' );
@@ -74,4 +80,3 @@ for ($F = 0;$F<mysqli_num_rows($Query);$F++)
 
 $pdf->Output("$pathPDF/multas/$d.pdf", "F");
  ?>
-
